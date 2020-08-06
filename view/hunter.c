40c8630abf71a551692187bbbcac7cbe1066358e
z5252826 <uzaifsheikh2309@gmail.com>
2020-08-06 11:19:43 +1000

    created hunter AI need to add more cases and test it

--- view/hunter.c
+++ view/hunter.c
@@ -12,9 +12,51 @@
 #include "Game.h"
 #include "hunter.h"
 #include "HunterView.h"
+#include <string.h>
 
 void decideHunterMove(HunterView hv)
 {
-	// TODO: Replace this with something better!
-	registerBestPlay("TO", "Have we nothing Toulouse?");
+	//int round = HvGetRound(hv);
+	Player curr_player = HvGetPlayer(hv);
+	PlaceId curr_loc_hunter = HvGetPlayerLocation(hv,curr_player);
+	PlaceId vampire_loc = HvGetVampireLocation(hv);
+
+	int numPlace = 0;
+	PlaceId* where_dracula_can_go = HvWhereCanTheyGo(hv,PLAYER_DRACULA,&numPlace);
+	char city[2];
+
+	if(HvGetHealth(hv,curr_player) <= 4 && HvGetHealth(hv,curr_player) != 0){
+		strcpy(city,placeIdToAbbrev(curr_loc_hunter));
+	}
+	else if(HvGetHealth(hv,curr_player) == 0){
+		strcpy(city,placeIdToAbbrev(HOSPITAL_PLACE));
+	}
+	else if(where_dracula_can_go == NULL && vampire_loc == NOWHERE){
+		int numPlace1 = 0;
+		PlaceId* where_i_can_go = HvWhereCanIGo(hv,&numPlace1);
+		if(numPlace1 > 0){
+			strcpy(city,placeIdToAbbrev(where_i_can_go[0]));
+		}
+
+	}
+	else if(where_dracula_can_go == NULL && vampire_loc != NOWHERE){
+
+		int numPlace2 = 0;
+		PlaceId* shortest_path = HvGetShortestPathTo(hv,curr_player,vampire_loc,&numPlace2);
+		if(numPlace2 > 0){
+			strcpy(city,placeIdToAbbrev(shortest_path[0]));
+		}
+
+	}
+	else if(where_dracula_can_go != NULL){
+		if(numPlace > 0){
+			int numPlace2 = 0;
+			PlaceId* shortest_path = HvGetShortestPathTo(hv,curr_player,where_dracula_can_go[0],&numPlace2);
+			if(numPlace2 > 0){
+				strcpy(city,placeIdToAbbrev(shortest_path[0]));
+			}
+		}
+	}
+
+	registerBestPlay(city, "Have we nothing Toulouse?");
 }
